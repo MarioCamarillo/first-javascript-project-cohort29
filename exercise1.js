dcbf60415aca990f2576d0fea27c4b1b2d60b19f
MarioCamarillo <104475782+MarioCamarillo@users.noreply.github.com>
2022-05-15 11:25:16 -0700

    Creating variables
    We were creating variables with let and const

---
+++ exercise1.js
@@ -0,0 +1,18 @@
+////////////////////////////////////
+// Variables for:
+// jobTitle
+// geoLocation
+// partnerName
+// numberChildren
+////////////////////////////////////
+
+let jobTitle = "Electronic Engineer";
+let geoLocation = "Mexicali, Baja California, México";
+let partnerName = "Ivonn";
+let numberChildren = 1;
+
+
+////////////////////////////////////////////////////
+// print to the console the information
+////////////////////////////////////////////////////
+document.writeln(`<p>You'll be a ${jobTitle}, in ${geoLocation}, and married to ${partnerName} with ${numberChildren} kid</p>`);
