6755eaf5ba2448fe391eac6cc132fe613d8ca9ef
MarioCamarillo <104475782+MarioCamarillo@users.noreply.github.com>
2022-05-15 11:46:28 -0700

    Creating operations with variables
    This will be used to make more variables

---
+++ exercise2.js
@@ -0,0 +1,22 @@
+////////////////////////////////////
+// Variables for:
+// name  <--this will be userName
+// email
+// password
+// age
+// country
+// salary (monthly)
+////////////////////////////////////
+
+let userName = "Mario Alberto Camarillo Ramos";
+let email = "mario.camarillo@somewhereintheworld.com";
+let password = "some_password_with_letters_and_numbers";
+let age = 41;
+let country = "México";
+let salary = 1234567890;
+
+
+////////////////////////////////////////////////////
+// print to the console the information
+////////////////////////////////////////////////////
+document.writeln(`<p>The name is ${userName}, with an email like this ${email}, and a annualy salary of ${salary*12}</p>`);
